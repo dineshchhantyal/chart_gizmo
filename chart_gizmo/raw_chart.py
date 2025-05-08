bc6bec2d633975acdc163d49642e5cfbcd613afc
Aaron Watters <awatters@flatironinstitute.org>
2025-05-08 15:27:05 -0400

    snapshot api

--- chart_gizmo/raw_chart.py
+++ chart_gizmo/raw_chart.py
@@ -1,6 +1,6 @@
 
 #from H5Gizmos import Html, Text, Button, Stack, serve, do
-from H5Gizmos import jQueryComponent, do
+from H5Gizmos import jQueryComponent, do, get
 import importlib.resources
 
 class RawChart(jQueryComponent):
@@ -17,12 +17,36 @@ class RawChart(jQueryComponent):
         dom_canvas = element[0].querySelector('*')
         Chart = self.window.chart_gizmo_js.Chart
         console = self.window.console
-        do(console.log("test log", self.window))
-        do(console.log("chart_gizmo_js loaded", self.window.chart_gizmo_js))
-        do(console.log("Chart loaded", Chart))
-        do(console.log("dom_canvas", dom_canvas))
+        #do(console.log("test log", self.window))
+        #do(console.log("chart_gizmo_js loaded", self.window.chart_gizmo_js))
+        #do(console.log("Chart loaded", Chart))
+        #do(console.log("dom_canvas", dom_canvas))
         my_chart = self.cache("my_chart", self.new(Chart, dom_canvas, configuration))
         self.chart = my_chart
+    async def getBase64URL(self, type=None, quality=1):
+        if type is not None:
+            return await get(self.chart.toBase64Image(type, quality))
+        else:
+            return await get(self.chart.toBase64Image())
+    async def getBase64ImageData(self):
+        # only for png
+        url = await self.getBase64URL()
+        prefix = "data:image/png;base64,"
+        assert url.startswith(prefix), f"Invalid base64 image data: {url[:40]}..."
+        base64_data = url[len(prefix):]
+        return base64_data
+    async def getImageBinary(self):
+        import base64
+        base64_data = await self.getBase64ImageData()
+        # decode the base64 data
+        binary_data = base64.b64decode(base64_data)
+        # convert to bytes
+        return bytes(binary_data)
+    async def saveImage(self, filename):
+        # save the image to a file
+        binary_data = await self.getImageBinary()
+        with open(filename, "wb") as f:
+            f.write(binary_data)
 
 # example usage
 true = True
@@ -69,9 +93,26 @@ def example_bar_chart():
     return chart
 
 def serve_example():
-    from H5Gizmos import serve
+    from H5Gizmos import serve, Button, Stack, Html, schedule_task, Text
+    # create a button to get the base64 image
+    def button_clicked(*arguments):
+        schedule_task(snapshot())
+    async def snapshot():
+        # get the base64 image
+        base64_image = await chart.getBase64URL()
+        txt = repr(base64_image[:40])
+        display.add(Text(f"Base64 image: {txt}..."))
+        # create an image tag with the base64 image
+        img_tag = f'<img src="{base64_image}" />'
+        # show the image in a new window
+        snap = Html(img_tag)
+        display.add(snap)
+        await chart.saveImage("chart.png")
+        display.add(Text("Image saved as chart.png"))
+    button = Button("Get Base64 Image", on_click=button_clicked)
     chart = example_bar_chart()
-    serve(chart.show())
+    display = Stack([button, chart])
+    serve(display.show())
 
 if __name__ == "__main__":
     # run the example
