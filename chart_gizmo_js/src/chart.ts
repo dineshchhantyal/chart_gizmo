f16ebf865b10e45566dbf5e459144b3bd53bca76
Aaron Watters <awatters@flatironinstitute.org>
2025-05-12 14:39:28 -0400

    expose all standard plugins

--- chart_gizmo_js/src/chart.ts
+++ chart_gizmo_js/src/chart.ts
@@ -1,29 +1,59 @@
 import {
   Chart,
+  // Controllers
   BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+
+  // Elements
+  ArcElement,
   BarElement,
+  LineElement,
+  PointElement,
+
+  // Scales
   CategoryScale,
   LinearScale,
   LogarithmicScale,
-  Title,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+
+  // Plugins
+  Decimation,
+  Filler,
   Legend,
-  Tooltip,
-  ChartConfiguration
+  SubTitle,
+  Title,
+  Tooltip
 } from 'chart.js';
+
 import ChartDataLabels from 'chartjs-plugin-datalabels';
 import annotationPlugin from 'chartjs-plugin-annotation';
 
 Chart.register(
-  BarController,
-  BarElement,
-  CategoryScale,
-  LinearScale,
-  LogarithmicScale,
-  Title,
-  Legend,
-  Tooltip,
-  ChartDataLabels,
-  annotationPlugin
+  // Controllers
+  BarController, BubbleController, DoughnutController,
+  LineController, PieController, PolarAreaController,
+  RadarController, ScatterController,
+
+  // Elements
+  ArcElement, BarElement, LineElement, PointElement,
+
+  // Scales
+  CategoryScale, LinearScale, LogarithmicScale,
+  RadialLinearScale, TimeScale, TimeSeriesScale,
+
+  // Plugins
+  Decimation, Filler, Legend, SubTitle, Title, Tooltip,
+
+  // additional plugins
+  ChartDataLabels, annotationPlugin
 );
 
 export { Chart };
