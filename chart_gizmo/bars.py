3eb72c6dc8a9e34b4f62dc006077ba1099eb5439
Aaron Watters <awatters@flatironinstitute.org>
2025-05-12 13:54:50 -0400

    bar chart class and related functionaliy

---
+++ chart_gizmo/bars.py
@@ -0,0 +1,152 @@
+"""
+Bar chart wrappers for Chart.js.
+"""
+
+from . import raw_chart
+from . import data_config
+
+default_options = dict(
+    responsive=True,
+    maintainAspectRatio=False,
+    scales=dict(
+        x=dict(
+            stacked=True,
+        ),
+        y=dict(
+            stacked=True,
+        ),
+    ),
+)
+
+class BarChart(raw_chart.RawChart):
+    """
+    A class to represent a bar chart.
+    """
+    def __init__(
+            self, 
+            configuration=None, 
+            width=400, 
+            height=400, 
+            stacked=False, options=None):
+        super().__init__(configuration, width, height)
+        self.configuration = configuration
+        self.type = "bar"
+        self.data = None
+        self.stacked = stacked
+        # only allow data configuration if configuration is None
+        if configuration is None:
+            self.data = data_config.ChartData()
+        self.options = options
+        self.logarithmic = False
+
+    def add_label(self, label, values=()):
+        """
+        Add a label to the chart.
+        """
+        if self.data is None:
+            raise ValueError("Data configuration is fixed.")
+        # add a label to the chart
+        self.data.add_label(label, values)
+        return self
+    def add_dataset(self, dataset):
+        """
+        Add a dataset to the chart.
+        """
+        if self.data is None:
+            raise ValueError("Data configuration is fixed.")
+        # add a dataset to the chart
+        self.data.add_dataset(dataset)
+        return self
+    def add_data_values(self, label, values=(), background_color=None, border_color=None, border_width=1):
+        """
+        Add data values to the chart.
+        """
+        if self.data is None:
+            raise ValueError("Data configuration is fixed.")
+        # add data values to the chart
+        self.data.add_data_values(label, values, background_color, border_color, border_width)
+        return self
+    def get_configuration(self):
+        configuration = self.configuration
+        options = self.options or default_options
+        if self.stacked:
+            options["scales"]["x"]["stacked"] = True
+            options["scales"]["y"]["stacked"] = True
+        if configuration is None:
+            configuration = dict(
+                type=self.type,
+                data=self.data.as_dict(),
+                options=self.options or default_options,
+            )
+        return configuration
+ 
+class CSVBarChart(BarChart):
+    """
+    Create a bar chart from a CSV file.
+    """
+    def __init__(self, filename, width=400, height=400, stacked=False, options=None):
+        super().__init__(width=width, height=height, stacked=stacked, options=options)
+        self.filename = filename
+        self.configuration = None
+        self.data = None
+        self.stacked = stacked
+        self.options = options
+        self.load_csv(filename)
+    def load_csv(self, filename):
+        """
+        Load a CSV file and create a bar chart.
+        """
+        import csv
+        with open(filename, "r") as f:
+            reader = csv.reader(f)
+            # read the header
+            header = next(reader)
+            # read the data
+            data = []
+            for row in reader:
+                data.append(row)
+            # create a bar chart
+            self.data = data_config.ChartData()
+            # Assume the first column is the dataset name.
+            # Assume the rest of the columns are the data values.
+            # Only use the columns that are strictly numeric.
+            # The first row is the header.
+            dataset_name_header = header[0]
+            value_names = header[1:]
+            is_numeric_column = {name: True for name in value_names}
+            name_to_values = {name: [] for name in value_names}
+            for row in data:
+                for i, value in enumerate(row[1:]):
+                    name = value_names[i]
+                    name_to_values[name].append(value)
+                    try:
+                        float(value)
+                    except ValueError:
+                        is_numeric_column[name] = False
+            # create the labels for numeric columns
+            not finished
+
+def serve_example_bar_chart():
+    """
+    Serve an example bar chart.
+    """
+    # create a BarChart object
+    from H5Gizmos import serve
+    chart = BarChart()
+    def click_callback(event):
+        print("Click event:", event)
+        chart.add("click: " + repr(event))
+    chart.on_click_call(click_callback)
+    # add dataset names
+    for name in "ABCDEF":
+        chart.add_label(name)
+    # add labels and data values
+    chart.add_data_values("Red", [12, 1, 23, 5, 2, 3])
+    chart.add_data_values("Blue", [1, 19, 13, 15, 2, 13])
+    chart.add_data_values("Yellow", [2, 19, 3, 5, 12, 3])
+    # return the chart object
+    serve(chart.show())
+
+if __name__ == "__main__":
+    # example usage
+    serve_example_bar_chart()
---
+++ chart_gizmo/data_config.py
@@ -0,0 +1,134 @@
+
+
+"""
+Helpers for creating chart configurations.
+"""
+
+from . import raw_chart
+from . import color_list
+
+class Datum:
+    """
+    A class to represent a data value in a dataset.
+    """
+    def __init__(self, value, background_color=None, border_color=None):
+        self.value = value
+        self.backgroundColor = background_color
+        self.borderColor = border_color
+    def configure_colors(self, background_color, border_color):
+        """
+        Configure the colors for the datum.
+        """
+        if background_color is not None:
+            self.backgroundColor = background_color
+        if border_color is not None:
+            self.borderColor = border_color
+        return self
+
+class DataSet:
+    """
+    A class to represent a dataset for a chart.
+    """
+    def __init__(
+            self, 
+            label, values=(), 
+            background_color=None, 
+            border_color=None, 
+            border_width=1,
+            **kwargs,
+            ):
+        self.label = label
+        self.data = []
+        for value in values:
+            self.data.append(Datum(value))
+        self.backgroundColor = background_color
+        self.borderColor = border_color
+        self.borderWidth = border_width
+        # other configuration options
+        self.options = kwargs
+    def append_value(self, value, background_color=None, border_color=None):
+        """
+        Append a value to the dataset.
+        """
+        datum = Datum(value, background_color, border_color)
+        self.data.append(datum)
+        return self
+    def configure_color_index(self, index, background_alpha=0.2, border_alpha=1.0):
+        """
+        Configure the color index for the dataset.
+        """
+        if self.backgroundColor is None:
+            self.backgroundColor = color_list.indexed_rgbahtml(index, background_alpha)
+        if self.borderColor is None:
+            self.borderColor = color_list.indexed_rgbahtml(index, border_alpha)
+        return self
+    def as_dict(self, index=0):
+        """
+        Convert the dataset to a JSON compatible dictionary.
+        """
+        # make sure colors are set
+        self.configure_color_index(index)
+        # colorize all datum entries
+        for datum in self.data:
+            datum.configure_colors(self.backgroundColor, self.borderColor)
+        result = dict(
+            label=self.label,
+            data=[datum.value for datum in self.data],
+            backgroundColor=[datum.backgroundColor for datum in self.data],
+            borderColor=[datum.borderColor for datum in self.data],
+            borderWidth=self.borderWidth,
+        )
+        # add other options
+        result.update(self.options)
+        return result
+    
+class ChartData:
+    """
+    A class to represent the data for a chart.js Chart.
+    """
+    def __init__(self, labels=(), datasets=()):
+        self.labels = list(labels)
+        self.datasets = list(datasets)
+    def add_label(self, label, values=()):
+        """
+        Add a label to the chart data.
+        """
+        ndatasets = len(self.datasets)
+        # values should match the number of datasets
+        if len(values) != ndatasets:
+            raise ValueError(f"Number of values {len(values)} does not match number of datasets {ndatasets}")
+        # add the label
+        self.labels.append(label)
+        # add the values to each dataset
+        for i, dataset in enumerate(self.datasets):
+            dataset.append_value(values[i])
+    def add_dataset(self, dataset):
+        """
+        Add a dataset to the chart data.
+        """
+        # the size of the dataset should match the number of labels
+        if len(dataset.data) != len(self.labels):
+            raise ValueError(f"Dataset size {len(dataset.data)} does not match number of labels {len(self.labels)}")
+        # add the dataset
+        self.datasets.append(dataset)
+    def add_data_values(self, label, values, background_color=None, border_color=None, border_width=1):
+        """
+        Add data values to the chart data.
+        """
+        # the size of the values should match the number of labels
+        if len(values) != len(self.labels):
+            raise ValueError(f"Values size {len(values)} does not match number of labels {len(self.labels)}")
+        # create a new dataset
+        dataset = DataSet(label, values, background_color, border_color, border_width)
+        # add the dataset
+        self.add_dataset(dataset)
+    def as_dict(self):
+        """
+        Convert the chart data to a JSON compatible dictionary.
+        """
+        result = dict(
+            labels=self.labels,
+            datasets=[dataset.as_dict(i) for i, dataset in enumerate(self.datasets)],
+        )
+        return result
+
