50085e4efd8dca0959a078103bc75d316481329b
Dinesh Chhantyal <myagdichhantyal@gmail.com>
2025-05-30 12:37:28 -0400

    Refactor BarChart class to inherit from AbstractChart and streamline CSVBarChartScript functionality

--- chart_gizmo/bars.py
+++ chart_gizmo/bars.py
@@ -2,8 +2,9 @@
 Bar chart wrappers for Chart.js.
 """
 
-from . import raw_chart
+from .abstract_chart import AbstractChart
 from . import data_config
+from .cli import CSVChartCLI
 
 default_options = dict(
     responsive=True,
@@ -23,15 +24,15 @@ default_options = dict(
     ),
 )
 
-class BarChart(raw_chart.RawChart):
+class BarChart(AbstractChart):
     """
     A class to represent a bar chart.
     """
     def __init__(
-            self, 
-            configuration=None, 
-            width=400, 
-            height=400, 
+            self,
+            configuration=None,
+            width=400,
+            height=400,
             stacked=False, options=None):
         super().__init__(configuration, width, height)
         self.configuration = configuration
@@ -43,73 +44,14 @@ class BarChart(raw_chart.RawChart):
             self.data = data_config.ChartData()
         self.options = options
 
-    def clear(self):
-        """
-        Clear the chart data.
-        """
-        self.data = data_config.ChartData()
-        return self
-    
-    def update(self):
-        """
-        Update the chart data.
-        """
-        # update the chart data
-        self.update_data(self.data.as_dict())
-        return self
 
-    def add_label(self, label, values=()):
-        """
-        Add a label to the chart.
-        """
-        if self.data is None:
-            raise ValueError("Data configuration is fixed.")
-        # add a label to the chart
-        self.data.add_label(label, values)
-        return self
-    
-    def add_dataset(self, dataset):
-        """
-        Add a dataset to the chart.
-        """
-        if self.data is None:
-            raise ValueError("Data configuration is fixed.")
-        # add a dataset to the chart
-        self.data.add_dataset(dataset)
-        return self
-    
-    def add_data_values(self, label, values=(), background_color=None, border_color=None, border_width=1):
-        """
-        Add data values to the chart.
-        """
-        if self.data is None:
-            raise ValueError("Data configuration is fixed.")
-        # add data values to the chart
-        self.data.add_data_values(label, values, background_color, border_color, border_width)
-        return self
-    
-    def get_configuration(self):
-        configuration = self.configuration
-        options = self.options or default_options
-        if self.stacked is not None:
-            # set the stacked option
-            options["scales"]["x"]["stacked"] = self.stacked
-            options["scales"]["y"]["stacked"] = self.stacked
-        if configuration is None:
-            configuration = dict(
-                type=self.type,
-                data=self.data.as_dict(),
-                options=self.options or default_options,
-            )
-        return configuration
- 
 
 class TabularBarChart(BarChart):
     """
     Create a bar chart from a tabular data source.
     """
     def __init__(
-            self, 
+            self,
             dictionaries,
             label_column,
             value_column,
@@ -161,7 +103,7 @@ class CSVBarChart(TabularBarChart):
     Create a bar chart from a CSV file.
     """
     def __init__(
-            self, 
+            self,
             csv_file,
             label_column,
             value_column,
@@ -181,52 +123,9 @@ class CSVBarChart(TabularBarChart):
 
 def CSVBarChartScript():
     "Command line script to create a CSV bar chart."
-    import argparse
-    import sys
-    import os
-    from H5Gizmos import serve
-    import csv
-    parser = argparse.ArgumentParser(description="Create a bar chart from a CSV file.")
-    parser.add_argument("csv_file", help="CSV file to read.")
-    # other arguments are optional with None as default and abbreviations
-    parser.add_argument("-l", "--label_column", help="Label column name.")
-    parser.add_argument("-v", "--value_column", help="Value column name.")
-    parser.add_argument("-g", "--group_column", help="Group column name.")
-    parser.add_argument("-w", "--width", type=int, default=400, help="Width of the chart.")
-    #parser.add_argument("-h", "--height", type=int, default=400, help="Height of the chart.")
-    # get the arguments
-    args = parser.parse_args()
-    # check if the file exists
-    csv_file = args.csv_file
-    if not os.path.exists(csv_file):
-        print(f"File {csv_file} does not exist.")
-        sys.exit(1)
-    label_column = args.label_column
-    value_column = args.value_column
-    group_column = args.group_column
-    width = args.width
-    #height = args.height
-    # use the first 3 columns if not specified
-    with open(csv_file, "r") as f:
-        reader = csv.DictReader(f)
-        headers = reader.fieldnames
-        if label_column is None:
-            label_column = headers[0]
-        if value_column is None:
-            value_column = headers[1]
-        if group_column is None:
-            group_column = headers[2]
-    # create the chart
-    chart = CSVBarChart(
-        csv_file,
-        label_column=label_column,
-        value_column=value_column,
-        group_column=group_column,
-        width=width,
-        #height=height,
-    )
-    # serve the chart
-    serve(chart.show())
+    cli = CSVChartCLI(CSVBarChart)
+    cli.run()
+
 
 # Example usages below are for testing purposes only.
 
